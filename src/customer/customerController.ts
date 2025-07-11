90c4ddc95eb764232313b09f58fa299fe8e569e7
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-07-11 20:30:54 +0600

    add address end point

--- src/customer/customerController.ts
+++ src/customer/customerController.ts
@@ -3,23 +3,43 @@ import { Request } from "express-jwt";
 import customerModel from "./customerModel";
 
 export class CustomerController {
-  getCustomer = async (req:Request, res:Response) => {
-    const {sub: userId, firstName, lastName, email} = req.auth;
-    console.log('auth', req.auth);
+  getCustomer = async (req: Request, res: Response) => {
+    const { sub: userId, firstName, lastName, email } = req.auth;
+    console.log("auth", req.auth);
 
-    const customer = await customerModel.findOne({userId})
+    const customer = await customerModel.findOne({ userId });
 
-    if(!customer){
+    if (!customer) {
       const newCustomer = await customerModel.create({
         userId,
         firstName,
         lastName,
         email,
         addresses: [],
-      })
-      return res.json(newCustomer)
+      });
+      return res.json(newCustomer);
     }
+    res.json(customer);
+  };
 
-    res.json(customer)
-  }
+  addAddress = async (req: Request, res: Response) => {
+    const { sub: userId } = req.auth;
+
+    const customer = await customerModel.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        userId,
+      },
+      {
+        $push: {
+          addresses: {
+            text: req.body.address,
+            isDefault: false,
+          },
+        },
+      },
+      { new: true },
+    );
+    return res.json(customer);
+  };
 }
