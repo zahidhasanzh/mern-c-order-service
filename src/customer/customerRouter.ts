b5a1af74805d8f51e8942aff181c6fb504d42085
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-07-11 20:30:54 +0600

    add address end point

--- src/customer/customerRouter.ts
+++ src/customer/customerRouter.ts
@@ -8,5 +8,6 @@ const router = express.Router();
 const customerController = new CustomerController()
 
 router.get("/", authenticate, asyncWrapper(customerController.getCustomer))
+router.patch ("/addresses/:id", authenticate, asyncWrapper(customerController.addAddress))
 
 export default router
