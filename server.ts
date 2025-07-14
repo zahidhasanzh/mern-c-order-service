e0eebac6ba903ce9c3792b60d1fbe625848d62ad
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-07-14 21:57:30 +0600

    add kafka consumer

--- server.ts
+++ server.ts
@@ -2,13 +2,18 @@ import app from "./src/app";
 import config from "config";
 import logger from "./src/config/logger";
 import connectDB from "./src/config/db";
+import { MessageBroker } from "./src/types/broker";
+import { createMessageBroker } from "./src/common/factories/brokerFactory";
 
 const startServer = async () => {
   const PORT = config.get("server.port") || 5503;
+  let broker: MessageBroker | null = null;
 
   try {
     await connectDB();
-
+    broker = createMessageBroker();
+    await broker.connectConsumer();
+    await broker.consumeMessage(["product"], false);
     app
       .listen(PORT, () => console.log(`Listening on port ${PORT}`))
       .on("error", (err) => {
@@ -17,6 +22,9 @@ const startServer = async () => {
       });
   } catch (err) {
     logger.error("Error happened: ", err.message);
+    if (broker) {
+      await broker.disconnectConsumer();
+    }
     process.exit(1);
   }
 };
---
+++ src/common/factories/brokerFactory.ts
@@ -0,0 +1,13 @@
+import config from 'config'
+import { KafkaBroker } from "../../config/kafka";
+import { MessageBroker } from "../../types/broker";
+
+let broker: MessageBroker | null = null;
+
+export const createMessageBroker = (): MessageBroker => {
+  console.log('connecting to kafka broker');
+  if(!broker){
+    broker = new KafkaBroker('order-service', [config.get('kafka.broker')])
+  }
+  return broker
+}
